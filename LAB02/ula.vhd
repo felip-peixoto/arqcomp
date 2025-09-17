80194173dc1e146b2a90193a2a38910a26349c7d
Felipe UTFPR <felipediaspeixoto@alunos.utfpr.edu.br>
2025-09-17 11:18:53 -0300

    Criação do IGUAL e MAIOR; Organização do Código

--- LAB02/ula.vhd
+++ LAB02/ula.vhd
@@ -3,36 +3,63 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity ula is 
-    port {
+    port (
         entr_A      : in unsigned(15 downto 0);
         entr_B      : in unsigned(15 downto 0);
         selec_op    : in unsigned(1 downto 0); --Escolher entre 4 operações (2 Bits)
-        carry_soma  : out std_logic;
+        carry       : out std_logic;
+        overflow    : out std_logic;
+        zero        : out std_logic;
+        sinal       : out std_logic;
         saida       : out unsigned(15 downto 0)
-    };
+    )
 end entity;
 
 architecture arq of ula is
-    signal op_soma : unsigned(15 downto 0);
-    signal op_sub  : unsigned(15 downto 0);
-    signal op_and  : unsigned(15 downto 0);
-    signal op_or   : unsigned(15 downto 0);
+    signal op_soma    : unsigned(15 downto 0);
+    signal op_sub     : unsigned(15 downto 0);
+    signal op_igual   : unsigned(15 downto 0);
+    signal op_maior   : unsigned(15 downto 0);
 
-    signal soma_carry : unsigned(16 downto 0); --Utilizaremos para encontrar o carry
+    signal soma_extendida : unsigned(16 downto 0);
+    signal sub_extendida  : unsigned(16 downto 0);
+    signal carry_soma     : std_logic;
+    signal overflow_soma  : std_logic;
+    signal carry_sub      : std_logic;
+    signal overflow_sub   : std_logic;
 begin 
 
-    op_soma = entr_A +   entr_B;
-    op_sub  = entr_A -   entr_B;
-    op_and  = entr_A and entr_B;
-    op_or   = entr_A or  entr_B;
+    op_soma = entr_A + entr_B;
+    op_sub  = entr_A - entr_B;
+
+    soma_extendida <= ('0' & entr_A) + ('0' & entr_B);
+    sub_extendida  <= ('0' & entr_A) - ('0' & entr_B);
  
+    carry_soma    <= soma_extendida(16);
+    overflow_soma <= '1' when (entr_A(15) = entr_B(15)) and (entr_A(15) /= op_soma(15)) else '0';
+
+
+    carry_sub    <= not sub_extendida(16); 
+    overflow_sub <= '1' when (entr_A(15) /= entr_B(15)) and (entr_B(15) = op_sub(15)) else '0';
+
+
     saida <= op_soma when selec_op = "00" else 
              op_sub  when selec_op = "01" else
-             op_and  when selec_op = "10" else
-             op_or   when selec_op = "11" else
+             op_igual  when selec_op = "10" else
+             op_maior   when selec_op = "11" else
              "0000000000000000";
     
-    soma_extendida <= ('0' & entr_A) + ('0' & entr_B);
+    
+    carry <= carry_soma when selec_op = "00" else
+                  carry_sub  when selec_op = "01" else
+                  '0'; 
 
-    carry <= soma_extendida(16) when selec_op = "00" else '0';         
+    overflow <= overflow_soma when selec_op = "00" else
+                     overflow_sub  when selec_op = "01" else
+                     '0';
+
+    sinal <= saida(15);
+    
+    zero <= '1' when saida = 0 else '0';
+
 end architecture arq;
